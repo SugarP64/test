cae08bb8a095dbbce3fcfe0302c79ce6c1e3cc10
SugarP64 <d.sato.b6493@gmail.com>
2021-05-13 10:48:45 +0900

    てすと

---
+++ var.js
@@ -0,0 +1,26 @@
+// var x = 3;
+// var	y = 4;
+// var z = x + y;
+// console.log(z) ; 
+
+// alert("Hello Worild!!");  //アラート
+
+// console.log(12+3);
+// console.log(12 + 'A');
+// console.log(12 + 3 + 'A');
+// console.log('A' + 12 + 3);
+// console.log('A' + 12 * 3);
+// console.log('A' + (12 + 3) );
+
+//型がobjectになる.配列型ではない
+// textbook = { //複数行でも書ける
+//   title: "shun とサル",
+//   page: 1,
+//   author:"shun"
+// };
+// console.log(textbook.title);  //これもOK
+// console.log(textbook['title']);
+
+//描画域に出る
+//document.write("出したい要素")
+//document.write("<p>Hello</p>");
